0ce387c2950184e9cd297b42371dedb39382897e
Prashik Maghade <139534949+Prashik109@users.noreply.github.com>
2023-10-13 01:29:50 +0530

    Create exp8a.cpp

---
+++ exp8a.cpp
@@ -0,0 +1,20 @@
+// PRASHIK MAGHADE
+/*PRN: 22070123109
+Experiment-no: 8(a)*/
+
+#include <iostream>
+#include<string.h>
+using namespace std;
+
+int main() {
+    string name;
+    cout<<"Enter your name: ";
+    getline(cin,name);
+    cout<<"My name is: "<<name;
+
+return 0;
+}
+/*OUTPUT
+Enter your name: Rutuja 
+My name is: Rutuja
+*/
